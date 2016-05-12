5047c173158b8c1bb913694ba55af0a8d2420196
jezza101 <jezza101@gmail.com>
2016-05-12 10:01:42 +0100

    Update frontpage.php

--- frontpage.php
+++ frontpage.php
@@ -21,10 +21,26 @@ class CPT_AjaxPoweredFrontPage{
  
         function __construct() {
 
+ 
+																		 remove_action( 'genesis_loop', 'genesis_do_loop' );
+																		 add_action( 'genesis_loop', array($this,'my_custom_loop');
 
         }
 //--END CONSTRUCTOR
  
+		function my_custom_loop(){
+					 global $wp_query, $post;
+					 while( $wp_query->have_posts() ): $wp_query->the_post();
+					  $post_id      = get_the_id();
+					  $classes      = ' one-third';
+					  if( 0 == $wp_query->current_post || 0 == $wp_query->current_post % 3 )
+					   {$classes .= ' first';}
+					  echo '<div id="product-' . $wp_query->current_post . '" class="' . $classes . '">';
+					   echo "<a href='".get_permalink()."'>".get_the_title()."</a>";
+					   the_excerpt();
+					  echo "</div>";
+					endwhile;
+		}
 
 }   //END CLASS
 }//END EXISTS CHECK
